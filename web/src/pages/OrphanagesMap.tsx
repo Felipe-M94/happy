6bea38216a6ceffa15121bfa367f14a9ca811495
Felipe-M94 <felipe.silva.mendonca@gmail.com>
2020-10-14 11:31:45 -0300

    Map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -28,14 +28,11 @@ function OrphanagesMap() {
 
       <Map 
         center={[-23.6612901,-46.7970448]}
-        zoom={16.25}
+        zoom={14}
         style={{ width: '100%', height: '100%' }}
       
       >
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {/* <TileLayer 
-          url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}}`} 
-        /> */}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
       </Map>
 
       <Link to="" className="create-orphanage">
